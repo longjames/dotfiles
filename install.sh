2ba9b7d615c21f3331256d892ce3e1531de0ac50
房文同 <fangwentong2012@gmail.com>
2014-07-03 01:09:20 +0800

    增加zsh,tmux配置，修改vim配置

--- install.sh
+++ install.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-CURRENT_DIR=`pwd`
-TODAY=`date +%Y%m%d`
+BASE_DIR=`pwd`
+TODAY=`date +%Y%m%d%H%M%S`
 
 #安装字体文件
 echo "正在安装字体..."
 mkdir -p $HOME/.fonts
-ln -fs $CURRENT_DIR/fonts/* $HOME/.fonts/
+ln -fs $BASE_DIR/fonts/* $HOME/.fonts/
 fc-cache -vf #刷新系统字体缓存
 
-#建立bashrc链接
-echo "配置bash..."
-if [ -L $HOME/.bashrc ];then
-    unlink $HOME/.bashrc
-elif [ -f $HOME/.bashrc ];then
-    mv $HOME/.bashrc $HOME/.bashrc.$TODAY
+#建立zshrc链接
+echo "配置zsh..."
+if [ -L $HOME/.zshrc ];then
+    unlink $HOME/.zshrc
+elif [ -f $HOME/.zshrc ];then
+    mv $HOME/.zshrc $HOME/.zshrc.$TODAY
 fi
-ln -s $CURRENT_DIR/bashrc $HOME/.bashrc
+ln -s $BASE_DIR/zsh/zshrc $HOME/.zshrc
+
+#建立tmux配置链接
+echo "配置tmux..."
+if [ -L $HOME/.tmux.conf ];then
+    unlink $HOME/.tmux.conf
+elif [ -f $HOME/.tmux.conf ];then
+    mv $HOME/.tmux.conf $HOME/.tmux.conf.$TODAY
+fi
+ln -s $BASE_DIR/tmux/tmux.conf $HOME/.tmux.conf
 
 #建立terminator配置链接
+mkdir -p $HOME/.config/terminator
 echo "配置terminator..."
 if [ -L $HOME/.config/terminator/config ];then
     unlink $HOME/.config/terminator/config
 elif [ -f $HOME/.config/terminator/config ];then
     mv $HOME/.config/terminator/config $HOME/.config/terminator/config.$TODAY
 fi
-ln -s $CURRENT_DIR/terminator/config $HOME/.config/terminator/config
+ln -s $BASE_DIR/terminator/config $HOME/.config/terminator/config
 
 #建立gitconfig链接
 echo "配置Git..."
@@ -34,46 +44,46 @@ if [ -L $HOME/.gitconfig ];then
 elif [ -f $HOME/.gitconfig ];then
     mv $HOME/.gitconfig $HOME/.gitconfig.$TODAY
 fi
-ln -s $CURRENT_DIR/gitconfig $HOME/.gitconfig
+ln -s $BASE_DIR/git/gitconfig $HOME/.gitconfig
 
 #配置Vim
 echo "备份vim配置..."
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles
+for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.bundles
 do [ -L $i ] && unlink $i
 done
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles
+for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.bundles
 do [ -e $i ] && mv $i $i.$TODAY
 done
 
 echo "重新配置Vim..."
-ln -s $CURRENT_DIR/vimrc $HOME/.vimrc
-ln -s $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
-ln -s $CURRENT_DIR/vim/ $HOME/.vim
+ln -s $BASE_DIR/vim/vimrc $HOME/.vimrc
+ln -s $BASE_DIR/vim/vimrc.bundles $HOME/.vimrc.bundles
+ln -s $BASE_DIR/vim/ $HOME/.vim
 
 echo "安装插件..."
-if [ ! -e $CURRENT_DIR/vim/bundle/vundle ]
+if [ ! -e $BASE_DIR/vim/bundle/vundle ]
 then
     echo "安装Vundle..."
-    git clone https://github.com/gmarik/vundle.git $CURRENT_DIR/vim/bundle/vundle
+    git clone https://github.com/gmarik/vundle.git $BASE_DIR/vim/bundle/vundle
 else
     echo "升级Vundle..."
-    cd "$CURRENT_DIR/vim/bundle/vundle" && git pull origin master
+    cd "$BASE_DIR/vim/bundle/vundle" && git pull origin master
 fi
 
 echo "使用Vundle安装/升级插件..."
 system_shell=$SHELL
 export SHELL="/bin/sh"
-echo "正在从GitHub上下载插件..." >> $CURRENT_DIR/notice
-echo "请保持网络畅通，耐心等待 :-)" >> $CURRENT_DIR/notice
-vim -u $HOME/.vimrc.bundles $CURRENT_DIR/notice +BundleInstall! +BundleClean +qall
+echo "正在从GitHub上下载插件..." >> $BASE_DIR/notice
+echo "请保持网络畅通，耐心等待 :-)" >> $BASE_DIR/notice
+vim -u $HOME/.vimrc.bundles $BASE_DIR/notice +BundleInstall! +BundleClean +qall
 export SHELL=$system_shell
-rm $CURRENT_DIR/notice
+rm $BASE_DIR/notice
 
 echo "编译YouCompleteMe..."
 echo "请保持网络畅通，这可能会花费较长时间"
 echo "编译会占用很多系统资源 :-)"
 echo "若安装失败，请先检查依赖关系，再手动安装"
-cd $CURRENT_DIR/vim/bundle/YouCompleteMe/
+cd $BASE_DIR/vim/bundle/YouCompleteMe/
 bash -x install.sh --clang-completer
 
 #Vim配置完成
