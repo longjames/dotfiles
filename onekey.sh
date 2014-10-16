a75ca3f6b7126d728342f50fac32c5a0eb40fa8e
房文同 <fangwentong2012@gmail.com>
2014-10-16 18:58:44 +0800

    update

--- onekey.sh
+++ onekey.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+sudo apt-get update
 sudo apt-get install -y git
 git clone https://github.com/fangwentong/dotfiles.git
 cd dotfiles
 git checkout -t origin/ubuntu
 git submodule update --init
-sudo sh -x ubuntu.sh
+sh -x ubuntu.sh
 sh -x install.sh
